071c5d69e68961fb396b4b239dc69c95f6d7ff35
dtws <dtws@gigwork.net>
2017-10-13 15:10:35 -0400

    tests

--- test/test_coowners.py
+++ test/test_coowners.py
@@ -1,8 +1,10 @@
+import json
 from helper import web
 from default import with_context
-from default import db, Fixtures
+from mock import patch
+from factories import ProjectFactory
+from default import Test, db, Fixtures
 from pybossa.repositories import ProjectRepository
-
 
 class TestCoowners(web.Helper):
 
@@ -19,71 +21,79 @@ class TestCoowners(web.Helper):
         self.register(name="John2", email="john2@john.com",
                       password="passwd")
         self.signout()
+        self.signin()
+        res = self.app.get("/admin/users/addsubadmin/2", follow_redirects=True)
+        self.signout()
         self.signin(email="john2@john.com", password="passwd")
         self.new_project()
 
         res = self.app.get('/project/sampleapp/coowners', follow_redirects=True)
         assert "Manage Co-owners" in res.data, res.data
 
+        self.signout()
+        self.signin()
+
+        res = self.app.get('/project/sampleapp/coowners', follow_redirects=True)
+        assert "Manage Co-owners" in res.data, res.data
+
     @with_context
     def test_01_admin_and_owner_add_del_coowner(self):
-        """Test admin and owner can add and delete coowners"""
+        """Test admin and owner can add/del a subadmin to coowners"""
         self.register()
+        self.signin()
+        self.signout()
         self.register(name="John2", email="john2@john.com",
                       password="passwd")
         self.register(name="John3", email="john3@john.com",
                       password="passwd")
+        self.signout()
+        self.signin()
+        res = self.app.get("/admin/users/addsubadmin/2", follow_redirects=True)
+        res = self.app.get("/admin/users/addsubadmin/3", follow_redirects=True)
+        self.signout()
         self.signin(email="john2@john.com", password="passwd")
         self.new_project()
 
-        res = self.app.get('/project/sampleapp/add_coowner/John3',
-                           follow_redirects=True)
+        res = self.app.get('/project/sampleapp/addcoowner/3', follow_redirects=True)
         assert "John3" in res.data, res.data
 
-        res = self.app.get('/project/sampleapp/del_coowner/John3',
-                           follow_redirects=True)
+        res = self.app.get('/project/sampleapp/delcoowner/3', follow_redirects=True)
         assert "John3" not in res.data, res.data
 
         self.signout()
         self.signin()
 
-        res = self.app.get('/project/sampleapp/add_coowner/John3',
-                           follow_redirects=True)
+        res = self.app.get('/project/sampleapp/addcoowner/3', follow_redirects=True)
         assert "John3" in res.data, res.data
 
-        res = self.app.get('/project/sampleapp/del_coowner/John3',
-                           follow_redirects=True)
+        res = self.app.get('/project/sampleapp/delcoowner/3', follow_redirects=True)
         assert "John3" not in res.data, res.data
 
     @with_context
     def test_02_nonadmin_notowner_authenticated_user_cannot_add_del_coowners(self):
-        """
-        Test non admin/not an owner authenticated user cannot add and delete
-        coowners to a project
-        """
+        """Test non admin/not an owner authenticated user cannot add/del coowners to a project"""
         self.register()
+        self.signin()
+        self.signout()
         self.register(name="John2", email="john2@john.com",
                       password="passwd")
         self.register(name="John3", email="john3@john.com",
                       password="passwd")
+        self.signout()
         self.signin()
         self.new_project()
-        self.new_task(1)
-        res = self.app.get('/project/sampleapp/add_coowner/John2',
-                           follow_redirects=True)
-        assert "John2" in res.data, res.data
+        res = self.app.get("/admin/users/addsubadmin/2", follow_redirects=True)
+        res = self.app.get('/project/sampleapp/addcoowner/2', follow_redirects=True)
         self.signout()
-
         self.signin(email="john3@john.com", password="passwd")
-        res = self.app.get('/project/sampleapp/add_coowner/John3',
-                           follow_redirects=True)
-        res = self.app.get('/project/sampleapp/del_coowner/John2',
-                           follow_redirects=True)
-        self.signout()
 
+        res = self.app.get('/project/sampleapp/addcoowner/3', follow_redirects=True)
+        res = self.app.get('/project/sampleapp/delcoowner/2', follow_redirects=True)
+
+        self.signout()
         self.signin()
-        res = self.app.get('/project/sampleapp/coowners',
-                           follow_redirects=True)
+
+        res = self.app.get('/project/sampleapp/coowners', follow_redirects=True)
         assert "John2" in res.data, res.data
         assert "John3" not in res.data, res.data
 
